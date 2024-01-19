28d42faee07114db3b123b460afc1c33a4700edf
luvega-rj <luvegaronny@gmail.com>
2024-01-19 14:15:36 +0300

    done

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -5,4 +5,6 @@ from django.http import HttpResponse
 # request - response
 # request handler
 def say_hello(request):
-    return HttpResponse('Hello world')
+    x= 1 
+    y= 2
+    return render(request,'hello.html', {'name': 'luvega'})
